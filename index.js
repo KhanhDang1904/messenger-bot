d4ddb96a540f2abe4da2616e2b277ac4c9331735
anhkhanh19042002 <anhkhanh5539574@gmail.com>
2025-02-01 11:19:45 +0700

    init

--- index.js
+++ index.js
@@ -61,4 +61,4 @@ function handleMessage(senderID, receivedMessage) {
 }
 
 // Chạy server
-app.listen(443, () => console.log("Server is running on port 443"));
+app.listen(4000, () => console.log("Server is running on port 4000"));
